90db82f43ed5a444acbf412d1708dbd05d546135
Xiaoyu Hu <hufk@bc.edu>
2019-01-27 23:03:02 -0500

    2.4 - This version of you are awesome has a show message button to display "You are awesome!"

--- You are awesome 2!/ViewController.swift
+++ You are awesome 2!/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageShow: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageShow.text = "You are Awesome!"
+    }
 }
 
