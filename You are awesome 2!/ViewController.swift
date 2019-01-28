c1f72763da8c259cdde90c9ff279254e2aef282d
Xiaoyu Hu <hufk@bc.edu>
2019-01-27 22:55:09 -0500

    Initial Commit

---
+++ You are awesome 2!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You are awesome 2!
+//
+//  Created by Xiaoyu Hu on 1/27/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ You are awesome 2!Tests/You_are_awesome_2_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  You_are_awesome_2_Tests.swift
+//  You are awesome 2!Tests
+//
+//  Created by Xiaoyu Hu on 1/27/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import XCTest
+@testable import You_are_awesome_2_
+
+class You_are_awesome_2_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
